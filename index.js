9563c2f3c0faa3ea66b85dca136b4c79baad27f8
jplee0203 <jplee0203@gmail.com>
2018-02-10 14:45:24 -0800

    newly added 2/10

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const port = process.env.PORT || 10000;
 const server = require("http").Server();
 
-var io = require("socket.io") (server);
-var allusers = [];
+var io = require("socket.io")(server);
+
+var allUsers = [];
 
 io.on("connection", function(socket){
     console.log("connect");
-    allusers.push(socket.id);
-    console.log(allusers);
+    allUsers.push(socket.id);
     
     socket.emit("yourid", socket.id);
     
-    io.emit("userjoined", allusers);
+    io.emit("userjoined", allUsers);
     
     socket.on("mymove", function(data){
-    socket.broadcast.emit("newmove", data);    
+        socket.broadcast.emit("newmove", data);         
     });
     
-    
     socket.on("disconnect", function(){
-        var index = allusers.indexOf(socket.id);
-        allusers.splice(index, 1);
-        io.emit("userjoined", allusers);
-    
-
-    })
+        var index = allUsers.indexOf(socket.id);
+        allUsers.splice(index, 1);
+        io.emit("userjoined", allUsers);
+    });
 });
 
-server.listen(port,(err)=>{
+
+server.listen(port, (err)=>{
     if(err){
         console.log(err);
         return false;
     }
     
-    console.log("Port is running");
-})
+    console.log("port is running");
+});
